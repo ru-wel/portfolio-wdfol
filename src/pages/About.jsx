95b84369964697985624c496fc9c348fa8e35b28
welru <reuelchristian.sundiam04@gmail.com>
2025-02-10 03:59:51 +0800

    emailjs configured on contact page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,68 @@
+import { Link } from 'react-router-dom';
 import Nav from './Nav';
 
+import "../assets/styles/home.scss";
+
 const About = () => {
-    return(
-      <>
+  return(
+    <div className='container'>
       <Nav />
-      <p>THIS IS THE ABOUT PAGE</p>
-      </>
-    );
+      <div className="profile-card">
+        <img src="images/rcgs.jpg" alt="Profile" className="profile-image" width={'485px'} height={'350px'}/>
+        
+        <div className="logo-badge">
+          <Link to={"/"}>
+            <img src="images/wel-logo-final.png" alt="RCGS Logo" width={'100px'} height={'100px'}/>
+          </Link>
+        </div>
+        
+        <h1 className="profile-name">Reuel Christian Sundiam</h1>
+        <p className="profile-title">Backend | Frontend Developer</p>
+        
+        <div className="social-links">
+          <a href="https://www.facebook.com/reuelchristian.sundiam" target='_blank' className="social-icon"><i className="fab fa-facebook"></i></a>
+          <a href="https://github.com/ru-wel" target='_blank' className="social-icon"><i className="fab fa-github"></i></a>
+          <a href="https://www.linkedin.com/in/reuel-christian-sundiam" target='_blank' className="social-icon"><i className="fab fa-linkedin"></i></a>
+          <a href="mailto:reuelchristian.sundiam04@gmail.com" target='_blank' className="social-icon"><i className="fas fa-envelope"></i></a>
+        </div>
+        
+        <div className="button-container">
+          <button className="profile-button">
+            <a href="RCGS-PORTFOLIO.pdf" download>Download Resume</a>
+            <i className="fa-solid fa-cloud-arrow-down"></i>
+          </button>
+          <button className="profile-button">
+            <Link to={"/contact"}>
+              Contact Me
+            </Link>
+            <i className="fa-solid fa-id-card-clip"></i>
+          </button>
+        </div>
+      </div>
+
+      <div className="home-card">
+
+        <div className="window">
+            <div className="window-header">
+                <h1 className="window-title">About</h1>
+                <div className="window-controls">
+                    <div className="control-dot"></div>
+                    <div className="control-dot"></div>
+                    <div className="control-dot"></div>
+                </div>
+            </div>
+            <div className="window-content">
+              <h2>Personal Bio</h2>
+              <p>A brief overview of your background, skills, and interests.</p>
+            </div>
+        </div>
+
+        <div className="projects">
+          <h2>Technical Skills</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,45 @@
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 import Nav from "./Nav";
 
 const Contact = () => {
-    return(
-        <>
-          <Nav />
-          <p>THIS IS THE CONTACT PAGE</p>
-        </>
-    );
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(import.meta.env.VITE_EMAILJS_SERVICE_ID, import.meta.env.VITE_EMAILJS_TEMPLATE_ID, form.current, {
+        publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
+      })
+      .then(
+        () => {
+          alert('Email sent succesfully!')
+          console.log('SUCCESS!');
+          form.current.reset();
+        },
+        (error) => {
+          alert('An error has occured')
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
+  return(
+    <>
+      <Nav />
+      <p>THIS IS THE CONTACT PAGE</p>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="name" required/>
+        <label>Email</label>
+        <input type="email" name="email" required/>
+        <label>Message</label>
+        <textarea name="message" rows={'5'} required/>
+        <input type="submit" value="Send" />
+      </form>
+    </>
+  );
 }
 
 export default Contact;
