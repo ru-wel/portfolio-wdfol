61a1a46f6b395ffca6b77c48937348be3a927f6e
welru <reuelchristian.sundiam04@gmail.com>
2025-02-12 23:47:37 +0800

    contact page done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,6 +7,8 @@ import "../assets/styles/contact.scss";
 import Nav from "./Nav";
 
 const Contact = () => {
+
+  // emailjs config
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -33,17 +35,34 @@ const Contact = () => {
       <Nav />
       <ProfileCard />
 
-      <div className="contact-card">
-        <form ref={form} onSubmit={sendEmail}>
-          <label>Name</label>
-          <input type="text" name="name" required/>
-          <label>Email</label>
-          <input type="email" name="email" required/>
-          <label>Message</label>
-          <textarea name="message" rows={'5'} required/>
-          <input type="submit" value="Send" />
-        </form>
+      <div className="home-card">
+
+        <div className="window">
+          <div className="window-header">
+              <h1 className="window-title">Contact</h1>
+              <div className="window-controls">
+                  <div className="control-dot"></div>
+                  <div className="control-dot"></div>
+                  <div className="control-dot"></div>
+              </div>
+          </div>
+          <div className="window-content">
+            <p>I am always open to new opportunities and collaborations. If you have something in mind or any questions, feel free to contact me. We can discuss how we can work together to make your project a success. Feel free to reach out!<span> This form will directly send a message to my email!</span></p>
+          </div>
+        </div>
+
+        <div className="projects contact">
+          <form ref={form} className='form' onSubmit={sendEmail}>
+            <h2>Contact Form</h2>
+            <input type="text" name="name" placeholder='Name' className='input' required/>
+            <input type="email" name="email" placeholder='Email' className='input' required/>
+            <textarea name="message" rows={'5'} className='message-input' placeholder='Message' required/>
+            <input type="submit" value="Send Message →" className='button-confirm'/>
+          </form>
+        </div>
       </div>
+
+      
     </div>
   );
 }
